8017bd62bbaedd5da675529af3812c43e712a321
jeppe sinkbaek <jeppest2@gmail.com>
2019-11-24 16:44:11 +0100

    converted to radial velocity

--- radial_velocity.py
+++ radial_velocity.py
@@ -3,22 +3,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as scc
 
+plt.rcParams.update({'font.size': 22})  # Changes font size on all plots
+
 wlength, data, bjd, bar_rvc = rf.project1_txtload('filenames')
 
 # # Interpolate datasets
+
 # Wavelength interpolation steps build with equal velocity spacing
 c = scc.c / 1000  # speed of light in km/s
 wl_start, wl_end = wlength[-1, 0], wlength[-1, -1]
-deltav = 1.0  # km/s
+deltav = 0.25  # km/s
 step_amnt = np.log10(wl_end/wl_start) / np.log10(1.0 + deltav/c) + 1
 wl_intp = wl_start * (1.0 + deltav/c)**(np.linspace(1, step_amnt, step_amnt))
 
+# Interpolate with new wavelength steps
 data_intp = rf.interpol(wlength, data, wl_intp)
+
 
 # # Cross correlate
 corr = np.zeros(data_intp.shape)
 wl_shift = wl_intp - wl_intp[len(wl_intp)//2]
-for i in range(0, 4):
+for i in range(0, len(corr[:, 0])):
     corr[i, :] = rf.cross_correlate(data_intp[i, :], data_intp[-1, :], plot=False)[0]
-    plt.plot(wl_shift, corr[i, :])
+
+plt.plot(wl_shift, corr[0, :])
+plt.xlabel('Wavelength shift [Å]')
+plt.ylabel('Normalized cross correlation')
+plt.xlim([-10, 10])
 plt.show()
+
+# Velocity shift
+v_shift = np.linspace(-step_amnt//2, step_amnt//2, step_amnt) * deltav
+
+plt.plot(v_shift, corr[0, :])
+plt.xlabel('Radial velocity shift [km/s]')
+plt.ylabel('Normalized cross correlation')
+plt.xlim([-250, 250])
+plt.show()
+
--- radial_velocity_functions.py
+++ radial_velocity_functions.py
@@ -8,7 +8,6 @@ def project1_txtload(fileref):
     with open(os.path.join('Datafiles', fileref+'.txt'), 'r') as f:
         references = []
         for line in f:
-            print(line)
             references.append(line.split()[0])
 
     for i in range(0, len(references)):
@@ -33,11 +32,12 @@ def project1_txtload(fileref):
 
 def cross_correlate(a, b, spacing=None, plot=True):
     # Expects uniform and same spacing for a and b, or that spacing is calculated outside the function
-    a = 1-a
-    b = 1-b
+    a = np.mean(a)-a
+    b = np.mean(b)-b
     steps = None
     result = np.correlate(a, b, mode='same')
     result = result/np.max(result)
+
     if spacing is not None:
         steps = np.linspace(-len(result)//2 * spacing, len(result)//2 * spacing, len(result))
     if plot is True and spacing is not None:
@@ -46,6 +46,7 @@ def cross_correlate(a, b, spacing=None, plot=True):
     elif plot is True:
         plt.plot(result)
         plt.show()
+
     return result, steps
 
 
@@ -53,6 +54,5 @@ def interpol(xp, yp, x):
     y = np.zeros((len(yp[:, 0]), len(x)))
     for i in range(0, len(yp[:, 0])):
         y[i, :] = np.interp(x, xp[i, :], yp[i, :])
-        print(i)
     return y
 
