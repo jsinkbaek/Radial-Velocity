7df54b38d4448813f0d2d0e02e39fd9af5b6e47f
jeppe sinkbaek <jeppest2@gmail.com>
2019-11-29 16:05:46 +0100

    some improvements and fitting

--- radial_velocity.py
+++ radial_velocity.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as scc
 from detect_peaks import detect_peaks
+from scipy.optimize import curve_fit
+import lmfit
+
 
 plt.rcParams.update({'font.size': 22})  # Changes font size on all plots
 
@@ -42,33 +45,33 @@ for i in range(0, len(corr[:, 0])-1):
     peak_indx.append(detect_peaks(corr[i, :], mph=0.2))
 
 
-indx_max = np.empty((len(corr[:, 0])-1, ))
-indx_min = np.empty((len(corr[:, 0])-1, ))
+indx_pmax = np.empty((len(corr[:, 0]) - 1,))
+indx_pmin = np.empty((len(corr[:, 0]) - 1,))
 v_pmax = np.empty((len(corr[:, 0])-1, ))
 v_pmin = np.empty((len(corr[:, 0])-1, ))
 pmax = np.empty((len(corr[:, 0])-1, ))
 pmin = np.empty((len(corr[:, 0])-1, ))
-indx_max[:] = np.nan
-indx_min[:] = np.nan
+indx_pmax[:] = np.nan
+indx_pmin[:] = np.nan
 v_pmax[:] = np.nan
 v_pmin[:] = np.nan
 pmax[:] = np.nan
 pmin[:] = np.nan
 
 
-for i in range(0, len(indx_max)):
-    indx_max[i] = np.argmax(corr[i, peak_indx[i]])
-    v_pmax[i] = v_shift[peak_indx[i]][int(indx_max[i])]
-    pmax[i] = corr[i, peak_indx[i][int(indx_max[i])]]
+for i in range(0, len(indx_pmax)):
+    indx_pmax[i] = np.argmax(corr[i, peak_indx[i]])
+    v_pmax[i] = v_shift[peak_indx[i]][int(indx_pmax[i])]
+    pmax[i] = corr[i, peak_indx[i][int(indx_pmax[i])]]
     if len(corr[i, peak_indx[i]]) == 1:
-        pass
+        indx_pmin[i] = indx_pmax[i]
     else:
-        indx_min[i] = np.argmin(corr[i, peak_indx[i]])
-        v_pmin[i] = v_shift[peak_indx[i]][int(indx_min[i])]
-        pmin[i] = corr[i, peak_indx[i][int(indx_min[i])]]
+        indx_pmin[i] = np.argmin(corr[i, peak_indx[i]])
+    v_pmin[i] = v_shift[peak_indx[i]][int(indx_pmin[i])]
+    pmin[i] = corr[i, peak_indx[i][int(indx_pmin[i])]]
 
 plt.figure()
-for i in range(0, len(indx_max)):
+for i in range(0, len(indx_pmax)):
     plt.plot(v_shift, corr[i, :])
 plt.xlabel('Radial velocity shift [km/s]')
 plt.ylabel('Normalized cross correlation')
@@ -87,8 +90,59 @@ plt.plot(bjd_sort, v_pmax_sort, 'r*', bjd_sort, v_pmax_sort, 'r',
          bjd_sort, v_pmin_sort, 'b*', bjd_sort, v_pmin_sort, 'b')
 plt.show(block=False)
 
-v_ratio = v_pmax_sort / v_pmin_sort
+
+# # Phaseplot
+pguess = np.mean([56524.9-56513.8, 56524.6-56513.7])
+freq_guess = 1/pguess
+# rf.phaseplot(v_pmax_sort, bjd_sort, v_pmin_sort, bjd_sort, pguess=pguess)
+# plt.close()
+t_pmax = bjd_sort-bjd_sort[0]
+t_pmin = bjd_sort-bjd_sort[0]
+# t_pmin = np.delete(bjd_sort, np.argwhere(np.isnan(v_pmin_sort))) - bjd_sort[0]
+# v_pmin_sort = np.delete(v_pmin_sort, np.argwhere(np.isnan(v_pmin_sort)))
+system_peaks = np.append(v_pmax_sort, v_pmin_sort)
+system_mean = np.mean(system_peaks)
+
+
+smodel = lmfit.Model(rf.sine_dstar)
+print('parameter names: {}'.format(smodel.param_names))
+print('independent variables: {}'.format(smodel.independent_vars))
+params = smodel.make_params(a=50, b=50, freq=freq_guess, phase=1, const=system_mean)
+params['a'].set(min=0, max=100)
+params['b'].set(min=0, max=100)
+params['freq'].set(min=0.5*freq_guess, max=2*freq_guess)
+params['phase'].set(min=-np.pi, max=np.pi)
+params['const'].set(min=-100, max=0)
+# print(params)
+# print(params['freq'])
+
+# fitres_max = smodel.fit(v_pmax_sort, x=t_pmax, a=50, freq=freq_guess, phase=1, const=system_mean)
+# popt_pmax = fitres_max.best_values
+# fitres_min = smodel.fit(v_pmin_sort, x=t_pmin, a=50, freq=freq_guess, phase=1, const=system_mean)
+# popt_pmin = fitres_min.best_values
+fitres = smodel.fit(v_pmax_sort+v_pmin_sort, params, x=t_pmax)
+fit_vals = fitres.best_values
+fit_a, fit_b, fit_freq, fit_phase, fit_const = fit_vals['a'], fit_vals['b'], fit_vals['freq'], fit_vals['phase'], \
+                                               fit_vals['const']
+print(fit_vals)
+print(fit_a, fit_b)
 
 plt.figure()
-plt.plot(bjd_sort, v_ratio, 'b', bjd_sort, v_ratio, 'r*')
-plt.show()
+t_fit = np.linspace(bjd_sort[0], bjd_sort[-1], 500000) - bjd_sort[0]
+fit_pmax = rf.sine_const(t_fit, fit_a, fit_freq, fit_phase, fit_const)
+fit_pmin = rf.sine_const(t_fit, fit_b, fit_freq, fit_phase+np.pi, fit_const)
+plt.plot(t_pmax, v_pmax_sort, 'r*', t_fit, fit_pmax, 'r--', linewidth=0.5)
+plt.plot(t_pmin, v_pmin_sort, 'b*', t_fit, fit_pmin, 'b--', linewidth=0.5)
+fit_tot = rf.sine_dstar(t_fit, fit_a, fit_b, fit_freq, fit_phase, fit_const)
+
+crossings = detect_peaks((fit_pmax-fit_pmin)**2, valley=True)
+# plt.plot(t_fit[crossings], fit_pmax[crossings], 'k*')
+# plt.plot(t_fit[crossings], fit_pmin[crossings], 'k*')
+# print(((fit_pmax-fit_pmin)**2)[crossings])
+# print(np.std(((fit_pmax-fit_pmin)**2)[crossings]))
+plt.show(block=False)
+
+plt.figure()
+plt.plot(t_pmax, v_pmax_sort+v_pmin_sort, '*')
+plt.plot(t_fit, fit_tot)
+plt.show(block=True)
--- radial_velocity_functions.py
+++ radial_velocity_functions.py
@@ -56,3 +56,52 @@ def interpol(xp, yp, x):
         y[i, :] = np.interp(x, xp[i, :], yp[i, :])
     return y
 
+
+def phaseplot(x1, t1, x2, t2, pguess=1, repeat=True):
+    """
+    Creates a double star phase plot. Can be animated/interactive (meaning keyboard interaction changes it, or a simple
+    plot that exits after a single loop.
+    """
+    plt.rcParams.update({'font.size': 30})
+    loop = True
+    plt.ion()
+    period = pguess
+    stepsize = 0.25
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plot1, = ax.plot(np.mod(t1, period) / period, x1, 'b*')
+    plot2, = ax.plot(np.mod(t2, period) / period, x2, 'r*')
+    print('Input a to decrease period, d to increase, q to quit loop, s to halve stepsize, w to double '
+          'stepsize, and p to pass (repeat same)')
+    while loop is True:
+        print('period', period, ', stepsize', stepsize)
+        plot1.set_xdata(np.mod(t1, period) / period)
+        plot2.set_xdata(np.mod(t2, period) / period)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        fig.legend(['Period '+'%.3f' % period + ' days'])
+        ax.set_xlabel('Phase: (time modulus period) / period')
+        ax.set_ylabel('Cepheid relative luminosity (V1/S1)')
+        inpt = input()
+        if inpt == 'q':
+            loop = False
+        elif inpt == 'a':
+            period -= stepsize
+        elif inpt == 'd':
+            period += stepsize
+        elif inpt == 's':
+            stepsize = stepsize/2
+        elif inpt == 'w':
+            stepsize = stepsize*2
+        elif inpt == 'p':
+            pass
+        if repeat is False:
+            loop = False
+
+
+def sine_const(x, a, freq, phase, const):
+    return a*np.sin(2*np.pi*x*freq + phase) + const
+
+
+def sine_dstar(x, a, b, freq, phase, const):
+    return a*np.sin(2*np.pi*x*freq + phase) + b*np.sin(2*np.pi*x*freq + phase + np.pi) + 2*const
